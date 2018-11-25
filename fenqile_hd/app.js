c7bfc550b8a78b3668bd0c852243e123b72e5d7a
1806 <462937186@qq.com>
2018-11-25 22:14:19 +0800

    1

--- fenqile_hd/app.js
+++ fenqile_hd/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
+var rateRouter = require('./routes/rate');
 
 var app = express();
 
@@ -16,7 +17,9 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+	extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 // 解决跨域问题
@@ -28,6 +31,7 @@ app.use(function (req, res, next) {
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
+app.use('/rate', rateRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
